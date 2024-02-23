f8ea5e13797cd80904510c887ed84c77a1ba25b8
James Tung <james.tung@student.tdsb.on.ca>
2024-02-23 15:15:21 -0500

    yes

---
+++ src/PracticeTest.java
@@ -0,0 +1,37 @@
+/*
+ * TITLE: Practice Test
+ * NAME: James Tung
+ * DATE: 02/23/2024
+ * DESCRIPTION: Solutions for Practice Test 01 - Grade 11 Java Review.
+ */
+
+import java.util.Scanner;
+
+public class PracticeTest {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void toefl() {
+        // Constants
+        final int SPLIT = 30;
+
+        // Variables
+        Scanner sc = new Scanner(System.in);
+        int reading, listening, speaking, writing;
+        int total = 0;
+
+        // Input
+        System.out.println("Enter the scores for the following.");
+        System.out.print("Reading: ");
+        reading = sc.nextInt();
+        System.out.print("Listening: ");
+        listening = sc.nextInt();
+        System.out.print("Speaking: ");
+        speaking = sc.nextInt();
+        System.out.print("Writing: ");
+        writing = sc.nextInt();
+
+        //
+    }
+}
