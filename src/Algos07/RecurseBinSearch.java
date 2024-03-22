2b351e5f1beb5f6fbd979d6a41035e10d95499cb
James Tung <james.tung@student.tdsb.on.ca>
2024-03-22 13:54:24 -0400

    mmmmm no

--- src/Algos07/RecurseBinSearch.java
+++ src/Algos07/RecurseBinSearch.java
@@ -9,8 +9,18 @@ package Algos07;
 
 public class RecurseBinSearch {
     public static void main(String[] args) {
-        // Code
+        int[] arr = new int[]{1, 24, 66, 312, 523, 634, 1235, 2931, 12492, 25662, 32832};
+
+        System.out.println(binary(arr, 32832, 0, arr.length));
     }
 
-    public static void binary
+    public static int binary(int[] list, int item, int start, int end) {
+        System.out.println(start + "    " + end);
+        if (start >= end) return -1;
+
+        int pivot = (start + end) / 2;
+        if (list[pivot] == item)     return pivot;
+        else if (list[pivot] > item) return binary(list, item, start, pivot);
+        else                         return binary(list, item, pivot + 1, end);
+    }
 }
---
+++ src/Algos08/StringRecursion.java
@@ -0,0 +1,28 @@
+/*
+ * TITLE: Algorithms08 - String Recursion
+ * NAME: James Tung
+ * DATE: 03/22/2024
+ * DESCRIPTION: A collection of standard string methods, implemented using recursion.
+ */
+
+package Algos08;
+
+public class StringRecursion {
+    public static void main(String[] args) {
+        // Code
+    }
+
+    public static int length(String s) {
+        if (s.isEmpty()) return 0;
+
+        return length(s.substring(1)) + 1;
+    }
+
+    public static boolean equalsIgnoreCase(String s1, String s2) {
+        if (s1.charAt(0) != s2.charAt(0)) {
+            if (s1.substring(0, 0).toLowerCase().equals(s2.substring(0, 0).toLowerCase()))
+        }
+
+        return equalsIgnoreCase(s1.substring(1), s2.substring(1));
+    }
+}
